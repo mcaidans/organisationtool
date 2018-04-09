f07700acc5f8234860a933860d33df85ced41f79
Aidan McMillan <mcaidans@gmail.com>
2018-04-09 03:50:20 +0000

    Cleaning up some directories and setting up layouts

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	  	<base href="https://organisation-tool-mcaidans.c9users.io/public/" />
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	
+		<title>
+		  @section('title')
+			Organisation Tool
+		  @show
+		</title>
+
+	    <!-- Must have links -->
+		    <!-- Latest compiled and minified CSS -->
+	    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	    	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+	    	
+	    	<!-- Bootstrap core JavaScript ================================================== -->
+	    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    	<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+	    	
+	    	<!-- Latest compiled and minified JavaScript -->
+	    	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	    	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+		<!-- END -->
+	    	
+		<!-- Custom styles for this template -->
+	    	<link href="css/app.css" rel="stylesheet">
+	    	<script src="js/app.js"></script>
+	    <!-- END -->
+	
+
+	</head>
+	
+	<body>
+	
+		@section('page')
+		@show
+	
+	</body>
+</html>
